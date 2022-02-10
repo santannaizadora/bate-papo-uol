7bee7ea0a13144e8cdede3f4f4f606a6761afbb1
santannaizadora <santannaizadora@gmail.com>
2022-02-09 22:14:20 -0300

    tela de entrada

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let user = ''
+
+const enterChat = () => {
+    user = document.querySelector('.log-user').value
+    loadingToEnter()
+    axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', { name: user })
+        .then(successEnterChat)
+        .catch(errorEnterChat)
+}
+
+const successEnterChat = () => {
+    const chat = document.querySelector('.login-screen')
+    chat.classList.add('hide')
+}
+
+const errorEnterChat = () => {
+    const error = document.querySelector('.login-screen-error')
+    error.classList.remove('hide')
+    errorWhenLoadingToEnter()
+}
+
+const loadingToEnter = () => {
+    const login = document.querySelector('.login')
+    login.classList.add('hide')
+    const loading = document.querySelector('.loading')
+    loading.classList.remove('hide')
+}
+
+const errorWhenLoadingToEnter = () => {
+    const login = document.querySelector('.login')
+    login.classList.remove('hide')
+    const loading = document.querySelector('.loading')
+    loading.classList.add('hide')
+}
